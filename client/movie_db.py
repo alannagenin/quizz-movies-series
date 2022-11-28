1d3a909efebe7dffb16a35b44cc4a6a6f873ce1d
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2022-11-28 21:15:46 +0100

    Add API requests for TV series and people

--- client/movie_db.py
+++ client/movie_db.py
@@ -1,17 +1,45 @@
 from dotenv import load_dotenv
 import os
 from requests import get
-from utils.singleton import Singleton
+from abstract_request import AbstractRequest
 load_dotenv()
 
-class MovieDB(metaclass=Singleton):
+class MovieDB(AbstractRequest):
     def __init__(self) -> None:
         self.DATABASE_URL = os.environ.get('DATABASE_URL')
         self.API_KEY = os.environ.get('API_KEY')
 
-    def get_movie_info(self, movie_id: int = 999000):
+    def get_movie_info(self, movie_id: int = 1):
+        '''
+        Request to get movie info based on its id.
+        '''
         req = get(
                     str(self.DATABASE_URL) + "movie/" + str(movie_id) + "?api_key=" + str(self.API_KEY)
-                )
-        raw_request = req.json()
-        return raw_request
+                )   
+        if req.status_code == 200:
+            raw_request = req.json()
+            return raw_request
+    
+    def get_tv_series_info(self, tv_id: int = 1):
+        '''
+        Request to tv series movie info based on its id.
+        '''
+        req = get(
+                    str(self.DATABASE_URL) + "tv/" + str(tv_id) + "?api_key=" + str(self.API_KEY)
+                )   
+        if req.status_code == 200:
+            raw_request = req.json()
+            return raw_request
+    
+    def get_people_info(self, tv_id: int = 1):
+        '''
+        Request to get people info based on its id.
+        '''
+        req = get(
+                    str(self.DATABASE_URL) + "person/" + str(tv_id) + "?api_key=" + str(self.API_KEY)
+                )   
+        if req.status_code == 200:
+            raw_request = req.json()
+            return raw_request
+    
+
