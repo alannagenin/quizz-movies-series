58f96d100fba6270c2957d7851ec997ea5aed03a
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2022-11-29 09:49:41 +0100

    Add method for TV and movies credits for people

--- client/movie_db.py
+++ client/movie_db.py
@@ -31,12 +31,24 @@ class MovieDB(AbstractRequest):
             raw_request = req.json()
             return raw_request
     
-    def get_people_info(self, tv_id: int = 1):
+    def get_people_info(self, people_id: int = 1):
         '''
         Request to get people info based on its id.
         '''
         req = get(
-                    str(self.DATABASE_URL) + "person/" + str(tv_id) + "?api_key=" + str(self.API_KEY)
+                    str(self.DATABASE_URL) + "person/" + str(people_id) + "?api_key=" + str(self.API_KEY)
+                )   
+        if req.status_code == 200:
+            raw_request = req.json()
+            return raw_request
+        
+    
+    def get_people_credits(self, people_id: int = 1):
+        '''
+        Request to get people info based on its id.
+        '''
+        req = get(
+                    str(self.DATABASE_URL) + "person/" + str(people_id) + "/combined_credits?api_key=" + str(self.API_KEY) + "&language=en-US"
                 )   
         if req.status_code == 200:
             raw_request = req.json()
@@ -44,6 +56,7 @@ class MovieDB(AbstractRequest):
     
 if __name__ == "__main__":
     requests = MovieDB()
-    print(requests.get_movie_info(), "\n\n")
-    print(requests.get_tv_series_info(), "\n\n")
-    print(requests.get_people_info())
+    # print(requests.get_movie_info(), "\n\n")
+    # print(requests.get_tv_series_info(), "\n\n")
+    # print(requests.get_people_info(), "\n\n")
+    print(requests.get_people_credits())
