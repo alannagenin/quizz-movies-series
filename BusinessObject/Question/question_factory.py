849ba5f0bf7dec2fca552dcfbba5bdb0f9a1c0e6
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2022-12-10 11:35:08 +0100

    Question factory to create new question

---
+++ BusinessObject/Question/question_factory.py
@@ -0,0 +1,112 @@
+from BusinessObject.Question.abstract_question import AbstractQuestion
+from BusinessObject.Question.question_movie_genre import QuestionMovieGenre
+from BusinessObject.Question.question_movie_plot import QuestionMoviePlot
+from BusinessObject.Question.question_movie_release_date import QuestionMovieReleaseDate
+from BusinessObject.Question.question_series_genre import QuestionSeriesGenre
+from BusinessObject.Question.question_series_nb_episodes_total import QuestionSeriesNumberEpisodesTotal
+from BusinessObject.Question.question_series_nb_seasons import QuestionSeriesNumberSeasons
+from BusinessObject.Question.question_series_plot import QuestionSeriesPlot
+from utils.singleton import Singleton
+
+
+class QuestionFactory(metaclass=Singleton):
+    def instantiate_movie_question(
+            self, type_question, movie_title, original_movie_title,
+            budget, genres_name, release_date, spoken_languages, plot
+            ) -> AbstractQuestion:
+        question = None
+        if type_question == "movie genre":
+            question = QuestionMovieGenre(
+                movie_title=movie_title,
+                original_movie_title=original_movie_title,
+                budget=budget,
+                genres_name=genres_name,
+                release_date=release_date,
+                spoken_languages=spoken_languages,
+                plot=plot
+            )
+        elif type_question == "movie plot":
+            question = QuestionMoviePlot(
+                movie_title=movie_title,
+                original_movie_title=original_movie_title,
+                budget=budget,
+                genres_name=genres_name,
+                release_date=release_date,
+                spoken_languages=spoken_languages,
+                plot=plot
+            )
+        elif type_question == "movie release date":
+            question = QuestionMovieReleaseDate(
+                movie_title=movie_title,
+                original_movie_title=original_movie_title,
+                budget=budget,
+                genres_name=genres_name,
+                release_date=release_date,
+                spoken_languages=spoken_languages,
+                plot=plot
+            )
+        return question
+
+    def instantiate_series_question(
+                self, type_question, series_title, original_series_title,
+                first_air_date, last_air_date, nb_episodes_per_season,
+                nb_episodes_tot, nb_seasons, tv_host, genres_name,
+                spoken_languages, plot) -> AbstractQuestion:
+        question = None
+        if type_question == "series genre":
+            question = QuestionSeriesGenre(
+                series_title=series_title,
+                original_series_title=original_series_title,
+                first_air_date=first_air_date,
+                last_air_date=last_air_date,
+                nb_episodes_per_season=nb_episodes_per_season,
+                nb_episodes_tot=nb_episodes_tot,
+                nb_seasons=nb_seasons,
+                tv_host=tv_host,
+                genres_name=genres_name,
+                spoken_languages=spoken_languages,
+                plot=plot
+            )
+        elif type_question == "series number episodes total":
+            question = QuestionSeriesNumberEpisodesTotal(
+                series_title=series_title,
+                original_series_title=original_series_title,
+                first_air_date=first_air_date,
+                last_air_date=last_air_date,
+                nb_episodes_per_season=nb_episodes_per_season,
+                nb_episodes_tot=nb_episodes_tot,
+                nb_seasons=nb_seasons,
+                tv_host=tv_host,
+                genres_name=genres_name,
+                spoken_languages=spoken_languages,
+                plot=plot
+            )
+        elif type_question == "series number seasons":
+            question = QuestionSeriesNumberSeasons(
+                series_title=series_title,
+                original_series_title=original_series_title,
+                first_air_date=first_air_date,
+                last_air_date=last_air_date,
+                nb_episodes_per_season=nb_episodes_per_season,
+                nb_episodes_tot=nb_episodes_tot,
+                nb_seasons=nb_seasons,
+                tv_host=tv_host,
+                genres_name=genres_name,
+                spoken_languages=spoken_languages,
+                plot=plot
+            )
+        elif type_question == "series plot":
+            question = QuestionSeriesPlot(
+                series_title=series_title,
+                original_series_title=original_series_title,
+                first_air_date=first_air_date,
+                last_air_date=last_air_date,
+                nb_episodes_per_season=nb_episodes_per_season,
+                nb_episodes_tot=nb_episodes_tot,
+                nb_seasons=nb_seasons,
+                tv_host=tv_host,
+                genres_name=genres_name,
+                spoken_languages=spoken_languages,
+                plot=plot
+            )
+        return question
