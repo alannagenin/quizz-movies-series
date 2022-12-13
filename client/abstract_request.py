ed9c98adc863f9a0236969c6d43461fe8574feac
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2022-12-13 12:58:17 +0100

    Add abstract methods to AbstractRequest

--- client/abstract_request.py
+++ client/abstract_request.py
@@ -1,8 +1,15 @@
 from abc import ABC, abstractmethod
 
-class AbstractRequest(ABC):
 
+class AbstractRequest(ABC):
     @abstractmethod
     def get_movie_info(self):
         pass
-    
+
+    @abstractmethod
+    def get_tv_series_info(self):
+        pass
+
+    @abstractmethod
+    def get_people_info(self):
+        pass
