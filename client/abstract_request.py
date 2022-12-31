cfff886b3b294d5a7936516b524a320c2dec5433
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2022-12-31 17:58:49 +0100

    Add ids to abstract methods in AbstractRequest

--- client/abstract_request.py
+++ client/abstract_request.py
@@ -3,13 +3,13 @@ from abc import ABC, abstractmethod
 
 class AbstractRequest(ABC):
     @abstractmethod
-    def get_movie_info(self):
+    def get_movie_info(self, movie_id):
         pass
 
     @abstractmethod
-    def get_tv_series_info(self):
+    def get_tv_series_info(self, tv_id):
         pass
 
     @abstractmethod
-    def get_people_info(self):
+    def get_people_info(self, people_id):
         pass
