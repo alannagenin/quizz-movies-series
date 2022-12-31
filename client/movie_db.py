9485bb0137b52530f5195c557508138b9026828a
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2022-12-31 18:00:05 +0100

    MovieDB: PEP8 and rename death date attribute

--- client/movie_db.py
+++ client/movie_db.py
@@ -13,7 +13,7 @@ class MovieDB(AbstractRequest):
         self.__DATABASE_URL = os.environ.get('DATABASE_URL')
         self.__API_KEY = os.environ.get('API_KEY')
 
-    def get_movie_info(self, movie_id: int) -> QuestionFactory:
+    def get_movie_info(self, movie_id: int):
         """
         Request to get movie info based on its id.
 
@@ -48,7 +48,10 @@ class MovieDB(AbstractRequest):
                 original_movie_title=raw_request['original_title'],
                 budget=raw_request['budget'],
                 genres_name=[raw_request['genres'][item]['name'] for item in range(len(raw_request['genres']))],
-                release_date=datetime.strptime(raw_request['release_date'], '%Y-%m-%d').year,
+                release_date=datetime.strptime(
+                    raw_request['release_date'],
+                    '%Y-%m-%d'
+                ).year,
                 spoken_languages=[raw_request['spoken_languages'][item]['english_name'] for item in range(len(raw_request['spoken_languages']))],
                 plot=raw_request['overview'],
                 type_question=choice(['movie genre', 'movie plot', 'movie release date'])
@@ -103,8 +106,7 @@ class MovieDB(AbstractRequest):
                 nb_seasons=len(raw_request['seasons']),
                 tv_host=[raw_request['networks'][item]['name'] for item in range(len(raw_request['networks']))],
                 genres_name=[raw_request['genres'][item]['name'] for item in range(len(raw_request['genres']))],
-                spoken_languages=[raw_request['spoken_languages'][item]['english_name'] for item in
-                                     range(len(raw_request['spoken_languages']))],
+                spoken_languages=[raw_request['spoken_languages'][item]['english_name'] for item in range(len(raw_request['spoken_languages']))],
                 plot=raw_request['overview'],
                 type_question=choice(['series genre', 'series number episodes total', 'series number seasons',
                                       'series plot'])
@@ -123,11 +125,11 @@ class MovieDB(AbstractRequest):
         Returns
         -------
         dict
-            returns a dictionnary containing the following information :
+            returns a dictionary containing the following information :
                 - name
                 - main role
                 - place of birth
-                - date of birth
+                - birthdate
                 - date of death
                 - is the person dead ?
         """
@@ -145,7 +147,7 @@ class MovieDB(AbstractRequest):
                 'main_role': raw_request['known_for_department'],
                 'place_birth': raw_request['place_of_birth'],
                 'date_birth': raw_request['birthday'],
-                'deathday': raw_request['deathday'],
+                'date_death': raw_request['deathday'],
                 'is_death': False if raw_request['deathday'] is None else True
                 }
             return result
