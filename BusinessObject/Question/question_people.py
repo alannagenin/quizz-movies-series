513065a4967b7600fd5584e94ef44d3644d00ae4
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2022-12-31 18:15:14 +0100

    Abstract question class for people

---
+++ BusinessObject/Question/question_people.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+from BusinessObject.Question.abstract_question import AbstractQuestion
+
+
+class QuestionPeople(AbstractQuestion, ABC):
+    def __init__(
+            self, name, main_role, place_birth, date_birth, date_death, is_death
+            ):
+        """
+        Create a new TV series question.
+
+        Parameters
+        ----------
+        self.name : string
+            Title of the series
+        self.main_role : string
+            Title of the movie in its original language
+        self.place_birth : string
+            Place of birth of the person
+        self.date_birth : Date
+            Date of the birth (yyyy-mm-dd format)
+        self.date_death : Date
+            If the person is dead then date of death (yyyy-mm-dd format) otherwise None
+        self.is_death : bool
+            Is the person death or not?
+
+        """
+        self.name = name
+        self.main_role = main_role
+        self.place_birth = place_birth
+        self.date_birth = date_birth
+        self.date_death = date_death
+        self.is_death = is_death
+        self.type_question = 'people'
+
+    @abstractmethod
+    def display_question(self):
+        """
+            Returns the question as it should be displayed in the quizz.
+        """
+        pass
+
+    @abstractmethod
+    def get_correct_answer(self):
+        """
+            Returns the correct answer of the corresponding question.
+        """
+        pass
